1b6736a7a07d7ec3a9bf505f740d3e1a34182b55
José Hilário Alves Neto <hilaneto@yahoo.com.br>
2026-08-16 20:07:19 -0300

    Página inicial e arquivo .css

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 
 from flask import Flask
+
 from routes.cpf import cpf_bp
 from routes.cnpj import cnpj_bp
+from routes.index import index_bp
+
 
 app = Flask(__name__)
 
 app.secret_key = '1553'
+
+app.register_blueprint( index_bp, url_prefix='/' )
 
 app.register_blueprint( cpf_bp, url_prefix='/cpf' )
 app.register_blueprint( cnpj_bp, url_prefix='/cnpj' )
