a5a9fad8cd064662fcef1e7afd23934ffb6d1304
José Hilário Alves Neto <hilaneto@yahoo.com.br>
2026-09-08 22:55:45 -0300

    ajuste de rotas

--- routes/auth.py
+++ routes/auth.py
@@ -1,27 +1,25 @@
 from flask import Blueprint, request, session, redirect, url_for, flash
+from models.usuario import Usuario
 
 auth_bp = Blueprint("auth", __name__)
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
-
     usuario = request.form.get("usuario")
     senha = request.form.get("senha")
-    # ---------------------------------------------------------
-    # SUA CONSULTA/VALIDAÇÃO REAL DO USUÁRIO
-    # ---------------------------------------------------------
-    # Exemplo:
-    # usuario_db = Usuario.autenticar(usuario, senha)
-    #
-    # if not usuario_db:
-    #     flash("Usuário ou senha inválidos.")
-    #     return redirect(url_for("home.home"))
-    # session["usuario_id"] = usuario_db.cd_usuario
-    # session["usuario_login"] = usuario_db.login
-    # ---------------------------------------------------------
+
+    usuario_db = Usuario.autenticar(usuario, senha)
+
+    if not usuario_db:
+        flash("Usuário ou senha inválidos.")
+        return redirect(url_for("home.home"))
+
+    session["usuario_id"] = usuario_db.cd_usuario
+    session["usuario_login"] = usuario_db.login
     return redirect(url_for("home.menu"))
 
 @auth_bp.route("/logout")
 def logout():
     session.clear()
+
     return redirect(url_for("home.home"))
